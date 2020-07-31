58c1a27f36cfa0c9c38064de34f4b6286a43aa76
ealin2 <ealin.chiu@gmail.com>
2020-07-31 19:04:46 +0800

    set Step-counting ALG as module

---
+++ ESP32_Test/m5stickc_demo2/step_counting.h
@@ -0,0 +1,12 @@
+#ifndef STEP_COUNTING_H__
+#define STEP_COUNTING_H__
+
+#define FILTER_N 12
+float Filter(char DIR) ;
+
+#define FILTER_N_3AIX 36
+
+void Filter_3aix(float * filterSum_X, float * filterSum_Y, float * filterSum_Z)  ;
+
+
+#endif
