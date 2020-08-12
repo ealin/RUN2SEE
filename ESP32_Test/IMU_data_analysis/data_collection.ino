04b13a9371addbb31839ca8567fc4ccbfd4b5abc
CHIU I-LING <ealin.chiu@gmail.com>
2020-08-13 01:37:39 +0800

    temp ver. - add code for open file & append data

--- ESP32_Test/IMU_data_analysis/data_collection.ino
+++ ESP32_Test/IMU_data_analysis/data_collection.ino
@@ -5,6 +5,9 @@ static int start_falg = 0 ;
 static File File_handle ;
 
 
+#define DATA_FILE "/acc_gyro_time.csv"
+
+
 // return 0 if open file in SD-card error
 //
 int start_collection(void) // open the file
@@ -42,7 +45,13 @@ int start_collection(void) // open the file
     SD_init_flag = 1 ;
  }   
 
- // open a new file
+ // open DATA_FILE in FILE_APPEND mode
+ File_handle = SD.open(DATA_FILE, FILE_APPEND);
+ if(!File_handle){
+        Serial.println("Failed to open file for appending");
+        return 0;
+ }    
+
  start_falg =1 ;  
  
  return 1 ;
@@ -51,13 +60,36 @@ int start_collection(void) // open the file
 
 void stop_collection(void)  // close the file
 {
+ File_handle.close(); 
  start_falg = 0 ; 
 }
 
 
 void sample_and_save_one_record(void)
 {
-  if(start_falg == 0)
+ if(start_falg == 0)
     return ;
+
+ float accX = 0.0F;
+ float accY = 0.0F;
+ float accZ = 0.0F;
+
+ float gyroX = 0.0F;
+ float gyroY = 0.0F;
+ float gyroZ = 0.0F;
+
+ char record_str[100] ;
+
+ M5.IMU.getGyroData(&gyroX,&gyroY,&gyroZ);
+ M5.IMU.getAccelData(&accX,&accY,&accZ);
+
+ sprintf(record_str,"%.5f,%.5f,%.5f,%.5f,%.5f,%.5f,%.5f\n",millis(),accX,accY,accZ,gyroX,gyroY,gyroZ) ;
+
+ File_handle.print(record_str) ;
+ if(File_handle.print(record_str)){
+    Serial.println("Message appended");
+ } else {
+    Serial.println("Append failed");
+} 
     
 }
